4fe2531b050bb95572bdd990df0e92ed83929b90
CasianLW <ciorbacasian@gmail.com>
2023-10-09 17:07:42 +0200

    crud for users (admin)

--- routes/auth.js
+++ routes/auth.js
@@ -91,6 +91,37 @@ router.post("/login", async (req, res) => {
   }
 });
 
+// Fetch all users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching users" });
+  }
+});
+// Create a new user
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error creating user" });
+  }
+});
+// Fetch a specific user by its ID
+router.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user" });
+  }
+});
 // Update user
 router.put("/user/:id", checkAdminKey, async (req, res) => {
   try {
